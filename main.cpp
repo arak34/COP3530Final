5167fda897eb5d6f8ce421910b26728228d9adf9
arak34 <101589789+arak34@users.noreply.github.com>
2023-04-26 16:45:15 -0400

    Update main.cpp
    trying to read in csv

--- main.cpp
+++ main.cpp
@@ -1,315 +1,77 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
 #include <string>
-#include <unordered_map>
-#include <cmath>
-#include <limits>
+#include <map>
+#include <iterator>
 
 struct Restaurant {
     std::string address;
-    bool alcohol;
-    bool caters;
-    bool dogs_allowed;
-    bool drive_through;
-    bool good_for_dancing;
-    bool good_for_kids;
-    bool happy_hour;
-    bool has_tv;
-    std::string noise_level;
-    bool open_24_hours;
-    bool outdoor_seating;
-    bool delivery;
-    bool good_for_groups;
-    int price_range;
-    bool reservation;
-    bool wifi;
+    std::string city;
+    std::string state;
+    std::string postal_code;
     std::string business_id;
     std::string categories;
-    std::string city;
-    std::string hours_friday;
-    std::string hours_monday;
-    std::string hours_saturday;
-    std::string hours_sunday;
-    std::string hours_thursday;
-    std::string hours_tuesday;
-    std::string hours_wednesday;
-    double latitude;
-    double longitude;
-    std::string name;
-    int postal_code;
     int review_count;
-    double stars;
-    std::string state;
+    float stars;
+    int is_open;
+    float latitude;
+    float longitude;
+    std::string name;
 };
 
-double haversine_distance(double lat1, double lon1, double lat2, double lon2) {
-    const double R = 6371.0;
-    double dLat = (lat2 - lat1) * M_PI / 180.0;
-    double dLon = (lon2 - lon1) * M_PI / 180.0;
-
-    double a = std::sin(dLat / 2) * std::sin(dLat / 2) +
-               std::cos(lat1 * M_PI / 180.0) * std::cos(lat2 * M_PI / 180.0) *
-               std::sin(dLon / 2) * std::sin(dLon / 2);
-
-    double c = 2 * std::atan2(std::sqrt(a), std::sqrt(1 - a));
-    return R * c;
-}
-
-std::vector<std::string> split(const std::string& s, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(s);
-    while (std::getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
-    }
-    return tokens;
-}
-
-bool str_to_bool(const std::string& s) {
-    return s == "True" || s == "true" || s == "1";
-}
-
-void read_csv(const std::string& file_name, std::unordered_map<std::string, Restaurant>& hash_table) {
-    std::ifstream file(file_name);
-    std::string line;
-
-    // Skip header line
-    std::getline(file, line);
-
-    while (std::getline(file, line)) {
-        std::vector<std::string> tokens = split(line, ',');
-
-        Restaurant r;
-        r.address = tokens[0];
-        r.alcohol = str_to_bool(tokens[1]);
-        r.caters = str_to_bool(tokens[2]);
-        r.dogs_allowed = str_to_bool(tokens[3]);
-        r.drive_through = str_to_bool(tokens[4]);
-        r.good_for_dancing = str_to_bool(tokens[5]);
-        r.good_for_kids = str_to_bool(tokens[6]);
-        r.happy_hour = str_to_bool(tokens[7]);
-        r.has_tv = str_to_bool(tokens[8]);
-        r.noise_level = tokens[9];
-        r.open_24_hours = str_to_bool(tokens[10]);
-        r.outdoor_seating = str_to_bool(tokens[11]);
-        r.delivery = str_to_bool(tokens[12]);
-        r.good_for_groups = str_to_bool(tokens[13]);
-        r.price_range = std::stoi(tokens[14]);
-        r.reservation = str_to_bool(tokens[15]);
-        r.wifi = str_to_bool(tokens[16]);
-        r.business_id = tokens[17];
-        r.categories = tokens[18];
-        r.city = tokens[19];
-        r.hours_friday = tokens[20];
-        r.hours_monday = tokens[21];
-        r.hours_saturday = tokens[22];
-        r.hours_sunday = tokens[23];
-        r.hours_thursday = tokens[24];
-        r.hours_tuesday = tokens[25];
-        r.hours_wednesday = tokens[26];
-        r.latitude = std::stod(tokens[27]);
-        r.longitude = std::stod(tokens[28]);
-        r.name = tokens[29];
-        r.postal_code = std::stoi(tokens[30]);
-        r.review_count = std::stoi(tokens[31]);
-        r.stars = std::stod(tokens[32]);
-        r.state = tokens[33];
-
-        hash_table[r.business_id] = r;
-    }
-}
-
 int main() {
-    std::unordered_map<std::string, Restaurant> hash_table;
-    read_csv("YelpRestaurants.csv", hash_table);
+    std::string csv_file = "YelpRestaurants.csv";
+    std::ifstream file(csv_file);
 
-    double user_latitude, user_longitude;
-    std::cout << "Enter your latitude: ";
-    std::cin >> user_latitude;
-    std::cout << "Enter your longitude: ";
-    std::cin >> user_longitude;
-
-    const double search_radius = 5.0; // Search radius in km
-    Restaurant best_restaurant;
-    double min_distance = std::numeric_limits<double>::max();
-
-    for (const auto& kv : hash_table) {
-        const Restaurant& r = kv.second;
-        double distance = haversine_distance(user_latitude, user_longitude, r.latitude, r.longitude);
-
-        if (distance <= search_radius && distance < min_distance) {
-            min_distance = distance;
-            best_restaurant = r;
-        }
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file" << std::endl;
+        return 1;
     }
 
-    if (min_distance == std::numeric_limits<double>::max()) {
-        std::cout << "No restaurants found within " << search_radius << " km radius." << std::endl;
-    } else {
-        std::cout << "Best restaurant found: " << best_restaurant.name << std::endl;
-        std::cout << "Address: " << best_restaurant.address << std::endl;
-        std::cout << "Distance: " << min_distance << " km" << std::endl;
+    std::string line;
+    std::getline(file, line); // Skip header line
+
+    std::map<std::string, Restaurant> restaurants;
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string token;
+
+        Restaurant restaurant;
+
+        std::getline(ss, restaurant.address, ',');
+        for (int i = 0; i < 42; ++i) {
+            std::getline(ss, token, ','); // Skip unnecessary columns
+        }
+        std::getline(ss, restaurant.business_id, ',');
+        std::getline(ss, restaurant.categories, ',');
+        std::getline(ss, restaurant.city, ',');
+        for (int i = 0; i < 6; ++i) {
+            std::getline(ss, token, ','); // Skip unnecessary columns
+        }
+        std::getline(ss, token, ',');
+        restaurant.is_open = std::stoi(token);
+        std::getline(ss, token, ',');
+        restaurant.latitude = std::stof(token);
+        std::getline(ss, token, ',');
+        restaurant.longitude = std::stof(token);
+        std::getline(ss, restaurant.name, ',');
+        std::getline(ss, restaurant.postal_code, ',');
+        std::getline(ss, token, ',');
+        restaurant.review_count = std::stoi(token);
+        std::getline(ss, token, ',');
+        restaurant.stars = std::stof(token);
+        std::getline(ss, restaurant.state, ',');
+
+        restaurants[restaurant.name] = restaurant;
+    }
+
+    file.close();
+
+    // Example: print all restaurant names
+    for (const auto& restaurant_pair : restaurants) {
+        std::cout << "Name: " << restaurant_pair.first << std::endl;
     }
 
     return 0;
-}
-
-// AVL Node structure
-struct AVLNode {
-    std::string key;
-    Restaurant value;
-    int height;
-    AVLNode* left;
-    AVLNode* right;
-
-    AVLNode(const std::string& key, const Restaurant& value)
-            : key(key), value(value), height(1), left(nullptr), right(nullptr) {}
-};
-
-class OrderedMap {
-public:
-    OrderedMap() : root(nullptr) {}
-
-    ~OrderedMap() {
-        clear(root);
-    }
-
-    void insert(const std::string& key, const Restaurant& value) {
-        root = insert(root, key, value);
-    }
-
-    void printInOrder() const {
-        printInOrder(root);
-    }
-
-private:
-    AVLNode* root;
-
-    int height(AVLNode* node) const {
-        return node ? node->height : 0;
-    }
-
-    int balanceFactor(AVLNode* node) const {
-        return height(node->left) - height(node->right);
-    }
-
-    void updateHeight(AVLNode* node) {
-        node->height = 1 + std::max(height(node->left), height(node->right));
-    }
-
-    AVLNode* rotateRight(AVLNode* y) {
-        AVLNode* x = y->left;
-        y->left = x->right;
-        x->right = y;
-
-        updateHeight(y);
-        updateHeight(x);
-
-        return x;
-    }
-
-    AVLNode* rotateLeft(AVLNode* x) {
-        AVLNode* y = x->right;
-        x->right = y->left;
-        y->left = x;
-
-        updateHeight(x);
-        updateHeight(y);
-
-        return y;
-    }
-
-    AVLNode* insert(AVLNode* node, const std::string& key, const Restaurant& value) {
-        if (!node) {
-            return new AVLNode(key, value);
-        }
-
-        if (key < node->key) {
-            node->left = insert(node->left, key, value);
-        } else if (key > node->key) {
-            node->right = insert(node->right, key, value);
-        } else {
-            // Key already exists, update the value
-            node->value = value;
-        }
-
-        updateHeight(node);
-
-        int balance = balanceFactor(node);
-
-        if (balance > 1) {
-            if (balanceFactor(node->left) < 0) {
-                node->left = rotateLeft(node->left);
-            }
-            return rotateRight(node);
-        }
-
-        if (balance < -1) {
-            if (balanceFactor(node->right) > 0) {
-                node->right = rotateRight(node->right);
-            }
-            return rotateLeft(node);
-        }
-
-        return node;
-    }
-
-    void printInOrder(AVLNode* node) const {
-        if (!node) {
-            return;
-        }
-
-        printInOrder(node->left);
-
-        const Restaurant& r = node->value;
-        std::cout << "Business ID: " << r.business_id << std::endl;
-        std::cout << "Name: " << r.name << std::endl;
-        std::cout << "Address: " << r.address << std::endl;
-        // Add any other fields you want to print
-        std::cout << "-----------------------------" << std::endl;
-
-        printInOrder(node->right);
-    }
-
-
-    void clear(AVLNode* node) {
-        if (!node) {
-            return;
-        }
-
-        clear(node->left);
-        clear(node->right);
-
-        delete node;
-    }
-};
-
-// Modify the read_csv function to use the OrderedMap
-void read_csv(const std::string& file_name, OrderedMap& ordered_map) {
-    std::ifstream file(file_name);
-    std::string line;
-
-    // Skip header line
-    std::getline(file, line);
-
-    while (std::getline(file, line)) {
-        std::vector<std::string> tokens = split(line, ',');
-
-        // ... (populate the Restaurant struct)
-
-        ordered_map.insert(r.business_id, r);
-    }
-}
-
-// Modify the main function to use the OrderedMap
-int main() {
-    OrderedMap ordered_map;
-    read_csv("restaurants.csv", ordered_map);
-
-    // Print the contents of the ordered_map to verify the data
-    ordered_map.printInOrder();
-
-    // ... (rest of the code)
 }
